111226ff25784163eaf0cc8798216d273d9e991f
Thotok Ary Septiawan <135796207+AryTokSep@users.noreply.github.com>
2023-06-29 00:11:23 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+// judul
+document.writeln("Kalkulator Sederhana")
+
+// isi
+let num1 = prompt("Masukkan angka pertama: ");
+let num2 = prompt("Masukkan angka kedua: ");
+
+let result;
+
+let operator = prompt("Masukkan operasi yang ingin Anda lakukan (+, -, *, /): ");
+
+if(operator === '+'){
+    result = parseInt(num1) + parseInt(num2);
+} else if(operator === '-'){
+    result = parseInt(num1) - parseInt(num2);
+} else if(operator === '*'){
+    result = parseInt(num1) * parseInt(num2);
+} else if(operator === '/'){
+    result = parseInt(num1) / parseInt(num2);
+} else {
+    alert("Hasil Tidak Diketahui!");
+}
+
+alert("Hasilnya adalah: " + result);
